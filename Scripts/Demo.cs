1937dcb3280a2fc6aa537bb3c996c7fc6f5eba11
Amhk17 <denx159357@gmail.com>
2018-01-01 04:23:06 +0100

    First Commit

---
+++ Scripts/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+	protected EnemyData enemyData;
+	void Start ()
+	{
+		// Load instance of /Settings/Enemies.csv from /Resources created by SettingsAutoConverter Class
+		LoadEnemy("Enemy1");
+		// Unload Database from assets, its optimum
+		UnloadEnemy();
+	}
+
+	private void LoadEnemy(string name){
+		enemyData = Resources.Load<EnemyData>(name);
+		Debug.Log(enemyData.name);
+	}
+
+	private void UnloadEnemy(){
+		enemyData = null;
+		Resources.UnloadUnusedAssets();
+	}
+}
